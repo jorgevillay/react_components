8205b2ac52ce1382e77c4a0b121c5127544ae574
Jorge Ivan Villay Jativa <Jorge.Villay@OMNICON.CC>
2020-02-24 11:19:00 -0500

    Focusable input functionality: Modified navigation to send focused prop

--- src/components/containers/MainNavigation.jsx
+++ src/components/containers/MainNavigation.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function MainNavigation() {
+  const [state, setState] = useState({
+    focused: false
+  })
+  const handleChange = event => {
+    const { checked } = event.target;
+    setState({
+      focused: checked
+    })
+  }
   return (
     <div>
       <h1>Jorge Iván Villay Játiva</h1>
       <ul>
-        <li><Link to="/focusable-input">Focusable Text Input</Link></li>
+        <li>
+          <input type="checkbox" name="focused" checked={state.focused} onChange={handleChange} />
+          <label>¿Initially focused input?</label>
+          <br />
+          <Link to={{ pathname: '/focusable-input', state: { focused: state.focused } }}>Focusable Text Input</Link>
+        </li>
         {/* Pending: Change the candidates number */}
         <li><Link to="/voting-list/0">Voting List</Link></li>
         <li><Link to="/register-form">Register Form</Link></li>
