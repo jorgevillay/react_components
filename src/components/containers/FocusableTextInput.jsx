cdb6e52a8505b42e77744d46a5e1a2dbc3d2025c
Jorge Ivan Villay Jativa <Jorge.Villay@OMNICON.CC>
2020-02-24 11:21:04 -0500

    Focusable input functionality: Modified focusable container to add prop control

--- src/components/containers/FocusableTextInput.jsx
+++ src/components/containers/FocusableTextInput.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import TextInput from '../TextInput';
 
-function FocusableTextInput() {
+function FocusableTextInput(props) {
+  let focusedInput = React.createRef();
+  const [state, setState] = useState({
+    focused: props.location.state.focused
+  })
+  const handleChange = event => {
+    const { checked } = event.target;
+    setState({
+      focused: checked
+    })
+  }
+  useEffect(() => {
+    state.focused && focusedInput.current.focus();
+  })
   return (
     <div>
       <h1>Welcome to FocusableTextInput</h1>
+      <input type="checkbox" name="focused" checked={state.focused} onChange={handleChange} />
+      <label>{props.location.state.focused ? '¿Is still focused?' : '¿Is focused?'}</label>
+      <br />
+      <TextInput ref={focusedInput} />
+      <br />
       <Link to="/">Return to main menu</Link>
-    </div>
+    </div >
   )
 }
 
