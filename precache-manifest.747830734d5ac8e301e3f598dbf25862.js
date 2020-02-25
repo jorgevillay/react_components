1616c68fc2b5682dbc49dd919e6beeaa8ffff895
Jorge Iván Villay Játiva <jorge_iv94@hotmail.com>
2020-02-25 02:56:58 -0500

    Updates

---
+++ precache-manifest.747830734d5ac8e301e3f598dbf25862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9991a7752eb90b041610e165c9c4617f",
+    "url": "/react_components/index.html"
+  },
+  {
+    "revision": "a2ec82d07c2af864aac5",
+    "url": "/react_components/static/css/main.4244d340.chunk.css"
+  },
+  {
+    "revision": "4072c3a6b217861772b2",
+    "url": "/react_components/static/js/2.169b90b1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_components/static/js/2.169b90b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ec82d07c2af864aac5",
+    "url": "/react_components/static/js/main.aa05c12b.chunk.js"
+  },
+  {
+    "revision": "eb968d6f3ccfec12f1b9",
+    "url": "/react_components/static/js/runtime-main.824e438e.js"
+  }
+]);
