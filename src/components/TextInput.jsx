7314d3a1463f568cab75048e470eabe8d01da186
Jorge Iván Villay Játiva <jorge_iv94@hotmail.com>
2020-02-25 01:06:11 -0500

    User interface: Modified FocusableTextInput container and Textnput component

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TextInput = React.forwardRef((props, ref) => (
-  <input type="text" ref={ref} />
+  <input type="text" className="form-control" ref={ref} />
 ));
 
 export default TextInput;
--- src/components/containers/FocusableTextInput.jsx
+++ src/components/containers/FocusableTextInput.jsx
@@ -17,15 +17,48 @@ function FocusableTextInput(props) {
     state.focused && focusedInput.current.focus();
   })
   return (
-    <div>
-      <h1>Welcome to FocusableTextInput</h1>
-      <input type="checkbox" name="focused" checked={state.focused} onChange={handleChange} />
-      <label>{props.location.state.focused ? '¿Is still focused?' : '¿Is focused?'}</label>
-      <br />
-      <TextInput ref={focusedInput} />
-      <br />
-      <Link to="/">Return to main menu</Link>
-    </div >
+    <div className="container-fluid pt-5">
+      <div className="row">
+        <div className="col-sm-6 offset-sm-3">
+          <div className="card text-center">
+            <div className="card-header">
+              <h5 className="card-title mb-0">React coding examples</h5>
+            </div>
+            <div className="card-body p-5">
+              <h3>Focusable text input</h3>
+              <ul className="no-bullet-list p-0 m-0 mt-5">
+                <li>
+                  <div className="row">
+                    <div className="col-sm-5 offset-sm-1">
+                      <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" name="focused" checked={state.focused} onChange={handleChange} />
+                        <label className="form-check-label">{props.location.state.focused ? '¿Is still focused?' : '¿Is focused?'}</label>
+                      </div>
+                    </div>
+                    <div className="col-sm-5">
+                      <TextInput ref={focusedInput} />
+                    </div>
+                  </div>
+                </li>
+                <hr />
+                <li className="mt-4">
+                  <div className="row">
+                    <div className="col-sm-6 offset-sm-3">
+                      <button type="button" className="btn btn-link">
+                        <Link to="/">Return to main menu</Link>
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="card-footer text-muted">
+              Developed using React and Formik
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
